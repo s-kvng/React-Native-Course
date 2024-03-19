de94ca321cd23ae08a249b2cde3f681326260d77
s-kvng <kvngnathan8420@gmail.com>
2024-03-19 22:09:52 +0000

    style inheritance

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -3,12 +3,17 @@ import { View, Text, StyleSheet } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.darkMode}>
+        <Text style={styles.darkModeText}>
+          Styles Inheritance <Text style={styles.boldText}> in Bold</Text>
+        </Text>
+      </View>
       <Text style={styles.title}>StyleSheet API</Text>
-      <View style={[styles.box, styles.lightblueBg]}>
+      <View style={[styles.box, styles.lightblueBg, styles.IOSboxShadow]}>
         {/* the last style in the array takes precedence */}
         <Text>Lightblue box</Text>
       </View>
-      <View style={[styles.lightgreenBg, styles.box]}>
+      <View style={[styles.lightgreenBg, styles.box, styles.andriodShadowBox]}>
         <Text>Lightgreen Box</Text>
       </View>
     </View>
@@ -23,8 +28,8 @@ const styles = StyleSheet.create({
     padding: 60,
   },
   box: {
-    width: "50%",
-    height: "25%",
+    width: "70%",
+    height: 250,
     // padding: 10,
     paddingHorizontal: 10,
     paddingVertical: 20,
@@ -40,6 +45,32 @@ const styles = StyleSheet.create({
     backgroundColor: "lightgreen",
   },
 
+  darkMode: {
+    backgroundColor: "black",
+  },
+
+  darkModeText: {
+    color: "white",
+  },
+
+  boldText: {
+    fontWeight: "800",
+  },
+
+  //works only for IOS devices
+  IOSboxShadow: {
+    shadowColor: "#333333",
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+  },
+  // works for android devices
+  andriodShadowBox: {
+    elevation: 10,
+  },
   title: {
     fontSize: 50,
   },
