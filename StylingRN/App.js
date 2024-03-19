a250d7b94a9086739e08a6029c3abb2bf943c17c
s-kvng <kvngnathan8420@gmail.com>
2024-03-19 21:44:42 +0000

    Multi styling and box modeling

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>StyleSheet API</Text>
+      <View style={[styles.box, styles.lightblueBg]}>
+        {/* the last style in the array takes precedence */}
+        <Text>Lightblue box</Text>
+      </View>
+      <View style={[styles.lightgreenBg, styles.box]}>
+        <Text>Lightgreen Box</Text>
+      </View>
     </View>
   );
 }
 
+// Multiple styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    padding: 60,
+  },
+  box: {
+    width: "50%",
+    height: "25%",
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginBottom: 10,
+    borderWidth: 2,
+    borderColor: "purple",
+    borderRadius: 10,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
+  },
+
+  title: {
+    fontSize: 50,
   },
 });
