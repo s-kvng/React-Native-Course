591f97b8918e72d5bd5ed33d12456127ea7e4f73
s-kvng <kvngnathan8420@gmail.com>
2024-03-21 13:23:16 +0000

    work on align content

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -8,9 +8,7 @@ export default function App() {
       <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>
-        Box 4
-      </Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803", alignSelf: "auto" }}>Box 5</Box>
       {/* when no align self is defined it takes it default value from the container , which has a default value of stretch  */}
       <Box style={{ backgroundColor: "#b95f43" }}>Box 6</Box>
@@ -25,7 +23,8 @@ const styles = StyleSheet.create({
     // flexDirection: "row",
     // justifyContent: "center",
     // alignItems: "baseline",
-    flexWrap: "wrap-reverse",
+    flexWrap: "wrap",
+    alignContent: "space-around",
     height: 300,
     marginTop: 64,
     borderWidth: 6,
