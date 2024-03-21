216f72dbb29d6c2a4ead4c88bbe5a9f80e9fac0a
s-kvng <kvngnathan8420@gmail.com>
2024-03-21 13:10:55 +0000

    work on alignSelf

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -5,23 +5,30 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#8e9b00", alignSelf: "flex-start" }}>
+        Box 1
+      </Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      {/* <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>
+        Box 4
+      </Box>
+      <Box style={{ backgroundColor: "#6b0803", alignSelf: "auto" }}>Box 5</Box>
+      {/* when no align self is defined it takes it default value from the container , which has a default value of stretch  */}
       <Box style={{ backgroundColor: "#b95f21" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 7</Box> */}
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: "row",
+    // flex: 1,
+    // flexDirection: "row",
     // justifyContent: "center",
-    alignItems: "baseline",
+    // alignItems: "baseline",
+
+    height: 300,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
