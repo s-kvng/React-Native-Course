299f2ba2c41c1833ef2edd67711f141335fbc16b
s-kvng <kvngnathan8420@gmail.com>
2024-03-21 13:14:50 +0000

    work on flex wrap

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -5,9 +5,7 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", alignSelf: "flex-start" }}>
-        Box 1
-      </Box>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>
@@ -15,8 +13,8 @@ export default function App() {
       </Box>
       <Box style={{ backgroundColor: "#6b0803", alignSelf: "auto" }}>Box 5</Box>
       {/* when no align self is defined it takes it default value from the container , which has a default value of stretch  */}
-      <Box style={{ backgroundColor: "#b95f21" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 7</Box>
+      <Box style={{ backgroundColor: "#b95f43" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#1c4c65" }}>Box 7</Box>
     </View>
   );
 }
@@ -27,7 +25,7 @@ const styles = StyleSheet.create({
     // flexDirection: "row",
     // justifyContent: "center",
     // alignItems: "baseline",
-
+    flexWrap: "wrap-reverse",
     height: 300,
     marginTop: 64,
     borderWidth: 6,
