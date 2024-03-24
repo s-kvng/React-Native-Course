cb2cdc022fb8d9a853d1cd3109dc6964f6bb80be
s-kvng <kvngnathan8420@gmail.com>
2024-03-24 21:53:50 +0000

    Fetch & loading state

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -1,20 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
+  const [postList, setPostList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=10`
+    );
+    const data = await response.json();
+    setPostList(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadContainer}>
+        <ActivityIndicator size={"large"} color={"#0000ff"} />
+        <Text style={{ marginTop: 15 }}>Loading...</Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={postList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.postCard}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.bodyText}>{item.body}</Text>
+              </View>
+            );
+          }}
+          ItemSeparatorComponent={<View style={{ height: 16 }}></View>}
+          ListEmptyComponent={
+            <Text
+              style={{ fontSize: 25, fontWeight: "bold", textAlign: "center" }}
+            >
+              No Post available
+            </Text>
+          }
+          ListHeaderComponent={
+            <Text
+              style={{
+                fontSize: 32,
+                fontWeight: "bold",
+                textAlign: "center",
+                paddingBottom: 12,
+              }}
+            >
+              POSTS
+            </Text>
+          }
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingHorizontal: 16,
+  },
+  loadContainer: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  postCard: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 5,
+    borderWidth: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  bodyText: {
+    fontSize: 15,
   },
 });
